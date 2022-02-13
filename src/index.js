4bc73bd2b8ac63b4026fb431b2fae95c64d647f5
NicoYue25 <nicole.yue25@gmail.com>
2022-02-13 13:12:09 -0500

    changed weather icons

--- src/index.js
+++ src/index.js
@@ -93,7 +93,6 @@ function getCurrentPosition() {
 function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
-  console.log(apiUrl);
 }
 
 //Current weather data in ºC
@@ -110,7 +109,7 @@ function displayCurrentInfo(response) {
     .querySelector("#current-icon")
     .setAttribute(
       "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      `images/WeatherIcons/${response.data.weather[0].icon}.png`
     );
 
   //temp
@@ -190,9 +189,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         ` <div class="col icons forecastBackground">
-      <img src="http://openweathermap.org/img/wn/${
-        forecastDay.weather[0].icon
-      }@2x.png"/>
+      <img src="images/WeatherIcons/${forecastDay.weather[0].icon}.png"/>
       </div>
               <div class="col info forecastBackground">
                 <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
